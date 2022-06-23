254298420062cea8f811460f896051abd882ebc9
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-23 23:28:23 +0900

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -24,7 +24,8 @@ class Vehicle {
   }
   
   public void printData() {
-    System.out.println("名前" + this.name);
+    System.out.println("名前：" + this.name);
     System.out.println("色：" + this.color);
-    System.out.println("走行距離：" + this.distance + "km+);
-                       }}
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+}
