91421b255ebf82b39a8a73679820f709b117adf8
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-24 21:38:33 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,13 @@ class Main {
     
     System.out.println("【車の情報】");
     car.printData();
+    System.out.println("ガソリン量：" + car.getFuel() + "L");
+    
+    System.out.println("-----------------");
+    System.out.print("給油する量を入力してください：");
+    int litre = scanner.nextInt();
+    car.charge(litre);
+    
     System.out.println("-----------------");
     System.out.println("【自転車の情報】");
     bicycle.printData();
