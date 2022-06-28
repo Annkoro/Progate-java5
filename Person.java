cbd5c627ae5c0657e19233ee240590ac41834013
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-29 08:17:09 +0900

    Update Person.java

--- Person.java
+++ Person.java
@@ -39,4 +39,10 @@ class Person {
     return this.weight/this.height/this.height;
   }
   
+  public void buy(Car car) {
+    car.setOwner(this);
+  }
   
+  public void buy(Bicycle bicycle) {
+    bicycle.setOwner(this);
+  }
